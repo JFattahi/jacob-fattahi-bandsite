e7490c62c0da28db8c2190d01453dc0acffccc66
Jacob Fattahi <jacobfattahi@gmail.com>
2025-02-09 02:24:42 -0400

    finish sprint-3

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -36,34 +36,6 @@ async function getCommentsArray() {
 }
 
 
-
-// Array holding the comments
-const commentsArray = [
-  {
-    commenterName: "Isaac Tadesse",
-    date: "10/20/2023",
-    comment:
-      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-    avatar: "../assets/images/avatar.png",
-  },
-  {
-    commenterName: "Christina Cabrera",
-    date: "10/28/2023",
-    comment:
-      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-    avatar: "../assets/images/avatar.png",
-  },
-  {
-    commenterName: "Victor Pinto",
-    date: "11/02/2023",
-    comment:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-    avatar: "../assets/images/avatar.png",
-  },
-];
-
-
-
 // Creates a comment block with a given comment, comment is an object in the array
 function createCommentBlock(comment) {
   const cardElement = document.createElement("article");
@@ -107,7 +79,7 @@ function createCommentBlock(comment) {
 
 // creates the comments article with the comments array
 function renderComments() {
-  const myCommentersElement = document.querySelector("#comments");
+  const myCommentersElement = document.getElementById("comments");
   myCommentersElement.innerHTML = "";
   commentsArray2
     .slice()
@@ -151,21 +123,6 @@ async function handleFormSubmit(event) {
   }
 
 
-
-
-  // const cardData = {
-  //   commenterName,
-  //   date: formatDate(new Date()),
-  //   comment: commentText,
-  // };
-
-  // commentsArray.push(cardData);
-
-  // const myCommentersElement = document.querySelector("#comments");
-  // const newCard = createCommentBlock(cardData);
-  // myCommentersElement.prepend(newCard);
-
-  
 }
 
 const formElement = document.getElementById("commenter-form");
